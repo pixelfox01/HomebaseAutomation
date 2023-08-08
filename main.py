b70fa54c377e867a7242395017fda19cafe4893b
Imran Yafith <imranyafith@gmail.com>
2023-08-08 01:09:08 -0400

    Set up Google Calendar API to add shift as an event to my Calendar.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+from calendar_service import get_calendar_service
+from datetime import datetime
 
 import keyring as kr
 
@@ -13,7 +15,22 @@ with sync_playwright() as p:
     page.is_visible('div.js-schedule')
     html = page.inner_html('div.js-schedule')
     soup = BeautifulSoup(html, 'html.parser')
+
+    calendar_service = get_calendar_service()
+
     for shift in soup.find_all('li', class_='js-profile-shift'):
-        print(f'Shift Date: {shift.find("div", class_="date").text.strip()}')
-        print(f'Shift Time: {shift.find("div", class_="time").text.strip()}')
-        print()
+        date = ' '.join(shift.find("div", class_="date").text.strip().split()[1:])
+        start_time = shift.find("div", class_="time").text.strip().split()[0]
+        end_time = shift.find("div", class_="time").text.strip().split()[2]
+        start_str = date + ' ' + start_time
+        end_str = date + ' ' + end_time
+        start = datetime.strptime(start_str, '%b %d, %Y %I:%M%p').isoformat()
+        end = datetime.strptime(end_str, '%b %d, %Y %I:%M%p').isoformat()
+
+        event_result = calendar_service.events().insert(calendarId='primary',
+                                                        body={
+                                                            "summary": 'HMC Athletics',
+                                                            "start": {"dateTime": start, "timeZone": 'America/Toronto'},
+                                                            "end": {"dateTime": end, "timeZone": 'America/Toronto'},
+                                                        }
+                                                        ).execute()
